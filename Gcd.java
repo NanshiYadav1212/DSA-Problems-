fd850e5539fecfb0c5dcb864cc08f7bc6f819548
NANSHI <nanshiyadav1212@gmail.com>
2025-07-22 02:33:24 +0000

    optimise

--- Gcd.java
+++ Gcd.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Gcd{
-    static int gcd(int n1,int n2)
+    static int gcd1(int n1,int n2)
     {
         int gcd=1;
         int n=Math.min(n1,n2);
@@ -11,12 +11,18 @@ public class Gcd{
         }
         return gcd;
     }
+    static int gcd2(int n1,int n2)
+    {
+        if(n2==0) return n1;
+        return gcd2(n2,n1%n2);
+    }
     public static void main(String a[])
     {
         Scanner sc=new Scanner(System.in);
         int n1=sc.nextInt();
         int n2=sc.nextInt();
-        int ans=gcd(n1,n2);
-        System.out.println("gcd of "+n1+" & "+n2 +" is "+ans);
+        int ans1=gcd1(n1,n2);
+        int ans2=gcd2(n1,n2);
+        System.out.println("gcd of "+n1+" & "+n2 +" is "+ans1 +" and "+ ans2);
     }
 }
